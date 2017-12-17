a4005f2b0486da3adb37a095e76f40673f73e52a
Andrew Garkavyi <andrew.gawy@gmail.com>
2017-12-17 11:42:47 -0800

    Code realignment, added test framework

---
+++ test/test_particle_filter.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Andrii Garkavyi on 12/8/17.
+//
+
+#include "gtest/gtest.h"
+#include "particle_filter.h"
+
+TEST(ParticleFilter, AugmentedSigmaPoints) {
+
+//  ASSERT_LT((Xsig_aug - Xsig_expected).norm(), 1e-5);
+}
